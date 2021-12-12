cbb681181c51c04a8e46f1605f7cf3bb3d8488b1
Duarte Roso <duarte.roso@posteo.eu>
2021-12-12 17:17:50 +0100

    Update module.v

--- module.v
+++ module.v
@@ -1,15 +1,11 @@
 module vglfw
 
 import duarteroso.semver
+import v.vmod
 
-// module_version returns the module semantic version
-pub fn module_version() semver.SemVer {
-	return semver.SemVer{
-		major: 0
-		minor: 2
-		patch: 2
-		stage: .alpha
-	}
+// manifest of module
+pub fn manifest() ?vmod.Manifest {
+	return vmod.decode(@VMOD_FILE)
 }
 
 // glfw_version return the GLFW semantic version
