d5b98fde7bdf14a58fc1620934b6ba6cfef6b475
Dinecraft Build <m81b4ck2@gmail.com>
2023-09-20 19:32:01 +0200

    Unsafe replace aswell

--- vidmode.v
+++ vidmode.v
@@ -25,7 +25,7 @@ pub:
 
 // create_vidmode creates a VideoMode instance
 pub fn create_vidmode(data voidptr) VideoMode {
-	raw_data := &C.GLFWvidmode(data)
+	raw_data := unsafe { &C.GLFWvidmode(data) }
 	v := VideoMode{}
 	C.glfwGetVidModeSizeHelper(raw_data, &v.width, &v.height)
 	C.glfwGetVidModeRGBBitsHelper(raw_data, &v.red_bits, &v.green_bits, &v.blue_bits)
