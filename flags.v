3465ca5179f8e1e129071df16b4e7d91d04408bd
Dinecraft Build <m81b4ck2@gmail.com>
2023-11-10 20:41:38 +0100

    Testing windows support + try to remove Vulkan for now

--- flags.v
+++ flags.v
@@ -1,14 +1,21 @@
 module vglfw
 
-#flag linux -I/usr/include/GLFW
-#flag linux -I@VROOT/include
-#flag linux -lglfw
+$if linux {
+	#flag linux -I/usr/include/GLFW
+	#flag linux -I@VROOT/include
+	#flag linux -lglfw
+	//#flag linux -DGLFW_INCLUDE_VULKAN=1
 
-#flag linux -DGLFW_INCLUDE_VULKAN=1
+	#include "GLFW/glfw3.h"
+	//#include "vulkan/vulkan.h"
+	#include "gamepadstate.h"
+	#include "gammaramp.h"
+	#include "image.h"
+	#include "vidmode.h"
+}
 
-#include "GLFW/glfw3.h"
-#include "vulkan/vulkan.h"
-#include "gamepadstate.h"
-#include "gammaramp.h"
-#include "image.h"
-#include "vidmode.h"
+$if windows {
+	#flag -I/usr/include/GLFW
+	#flag -I@VROOT/include
+	#flag -lglfw
+}
